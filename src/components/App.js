c218307b851e0b370c318137fde1243996aca5b5
Josh <pana23@gmail.com>
2020-03-31 21:43:01 +1100

    added the create a plane page

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CreatePlane from './CreatePlane';
+
+function App() {
+  return (
+     <div className="App">
+      <CreatePlane />
+     </div>
+  );
+}
+
+export default App;
