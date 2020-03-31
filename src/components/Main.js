dcde39b77aeae6a10ad46a68167655641e63b47a
Mitchell Browne <mitchellbrowne@Mitchells-MacBook-Air-2.local>
2020-03-31 21:35:51 +1100

    Added LogIn and SignUp pages with functional routing

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from './Home';
+import SignUp from './SignUp';
 
 const Main = () => {
   return (
     <Switch>
       <Route exact path='/' component={Home}></Route>
+      <Route exact path='/signup' component={SignUp}></Route>
     </Switch>
   );
 }
