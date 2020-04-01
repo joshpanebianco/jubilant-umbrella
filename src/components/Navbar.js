3e3b6d7015e07c38e062f9f4e339e3a954805655
Josh <pana23@gmail.com>
2020-04-01 11:47:39 +1100

    updates to the css for the nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,15 @@ import { Link } from 'react-router-dom';
 class Navbar extends Component {
   render() {
     return (
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/signup">Sign Up</Link>
-        <Link to="/login">Log In</Link>
-        <Link to="/airplanes">Create Airplane</Link>
-        <Link to="/flights">Create Flight</Link>
-        <Link to="/search">Search Flight</Link>
+      <div className="top-nav">
+        <div className="nav-list">
+          <Link to="/" className="nav-item">Home</Link>
+          <Link to="/signup" className="nav-item">Sign Up</Link>
+          <Link to="/login" className="nav-item">Log In</Link>
+          <Link to="/create-plane"className="nav-item">Create Airplane</Link>
+          <Link to="/flights" className="nav-item">Create Flight</Link>
+          <Link to="/search" className="nav-item">Search Flight</Link>
+        </div>
       </div>
     );
   }
