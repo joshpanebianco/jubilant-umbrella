c34cffabc4f9ee52b307cb5f8baaa9c115c7f99e
Mitchell Browne <mitchellbrowne@Mitchells-MacBook-Air-2.local>
2020-04-01 18:41:55 +1100

    Added signup and login forms

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class SignUp extends Component {
   render() {
     return (
-      <h1>Sign Up coming soon</h1>
+      <div>
+        <h2>Sign Up</h2>
+        <form>
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <input type="password" placeholder="confirm password" />
+          <input type="submit" />
+        </form>
+        <Link to='/login'>Already have an account?</Link>
+      </div>
     );
   }
 }
